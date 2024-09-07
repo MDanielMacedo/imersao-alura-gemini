45be9f7cc47af3b6404405f5590c0f99f3f18af4
Daniel Macedo <83512990+MDanielMacedo@users.noreply.github.com>
2024-09-06 22:48:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é um ícone da ginástica artística brasileira, tendo conquistado o status de maior medalhista olímpica do país. Sua trajetória inspiradora, marcada por superação de lesões e conquistas históricas, como o ouro no salto e a prata no individual geral nas Olimpíadas de Tóquio, a consolidaram como um exemplo de determinação e talento. Com sua performance brilhante nos Jogos de Paris 2024, onde conquistou mais quatro medalhas, Rebeca solidificou seu legado como uma das maiores atletas da história do Brasil, abrindo portas para futuras gerações de ginastas e encantando o mundo com sua paixão pelo esporte.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tag : "ouro, ginastica, ginástica"
+    },
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, conhecida como a 'Fadinha do Skate', é um fenômeno do skate brasileiro. Com sua juventude e talento impressionantes, conquistou a medalha de prata nas Olimpíadas de Tóquio 2020, tornando-se a medalhista olímpica mais jovem do Brasil. Sua alegria contagiante e habilidade no skate inspiram jovens skatistas em todo o mundo.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tag : "fadinha, fada, sk8, skatista"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira de destaque internacional. Com sua força e técnica apuradas, conquistou importantes títulos em sua carreira, incluindo medalhas em campeonatos mundiais e Jogos Pan-Americanos. Sua determinação e garra a tornam uma referência no judô brasileiro e uma inspiração para jovens atletas.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tag : "judoka, judo, judô"
+    } 
+]
+
+  
